333ec086e0edb036a62e0fab72e33eb667adb739
MinGyeol <alsruf741@naver.com>
2025-03-26 17:50:30 +0900

    로그인 안한 사용자도 로그인을 볼 수 있게 TokenCheckFilter 수정

--- src/main/java/com/example/lecture_B/security/filter/TokenCheckFilter.java
+++ src/main/java/com/example/lecture_B/security/filter/TokenCheckFilter.java
@@ -37,8 +37,13 @@ public class TokenCheckFilter extends OncePerRequestFilter {    // OncePerReques
         String path = request.getRequestURI();
 
         //토큰값이 없어도 들어갈 수 있는 페이지 //검색 API 요청은 인증 체크 제외
-        if (path.equals("/api/auth/signUp") || path.equals("/api/auth/signIn") ||path.startsWith("/api/boards/") && path.contains("/lectures/search")) {
+        if (path.equals("/api/auth/signUp") || path.equals("/api/auth/signIn")) {
             filterChain.doFilter(request, response);
+            return;
+        }
+
+        if (path.startsWith("/api/boards") || path.startsWith("/api/lectures")) {
+            filterChain.doFilter(request, response);  // 토큰 없이 통과
             return;
         }
 
--- src/main/java/com/example/lecture_B/security/handler/UserLoginSuccessHandler.java
+++ src/main/java/com/example/lecture_B/security/handler/UserLoginSuccessHandler.java
@@ -1,71 +1,71 @@
-//package com.example.lecture_B.security.handler;
-//
-//
-//import com.example.lecture_B.dto.SignUpDTO;
-//import com.example.lecture_B.entity.CustomUser;
-//import com.example.lecture_B.util.JwtUtil;
-//import com.google.gson.Gson;
-//import jakarta.servlet.ServletException;
-//import jakarta.servlet.http.HttpServletRequest;
-//import jakarta.servlet.http.HttpServletResponse;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.log4j.Log4j2;
-//import org.springframework.http.MediaType;
-//import org.springframework.security.core.Authentication;
-//import org.springframework.security.core.GrantedAuthority;
-//import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-//
-//import java.io.IOException;
-//import java.util.HashMap;
-//import java.util.Map;
-//import java.util.stream.Collectors;
-//
-//// 인증 성공 후 처리 작업을 담당
-//@Log4j2
-//@RequiredArgsConstructor
-//public class UserLoginSuccessHandler implements AuthenticationSuccessHandler {
-//
-//    private final JwtUtil jwtUtil;
-//
-//    @Override
-//    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-//        log.info("Login Success Handler.............");
-//
-//        // 응답 콘텐츠 타입을 JSON으로 설정
-//        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-//
-//        log.info(authentication.toString());
-//        log.info("Username: " + authentication.getName()); // username
-//
-//
-//        String userId = ((CustomUser) authentication.getPrincipal()).getUserId();
-//        log.info("Email: " + userId);
-//        CustomUser customUser = (CustomUser) authentication.getPrincipal();
-//        String nickname = customUser.getNickname();
-//        log.info("userId: " + userId);
-//
-//        // 클레임 생성 : JWT 토큰에 포함될 클레임(사용자 정보)을 생성
-//        // JWTUtil payload부분에서 .putall로 userid, nickname, roles 값이 들어감
-//        Map<String, Object> claim = new HashMap<>();
-//        claim.put("userId", userId);
-//        claim.put("nickname", nickname);
-//        claim.put("roles", authentication.getAuthorities().stream()
-//                .map(GrantedAuthority::getAuthority)
-//                .collect(Collectors.toList()));
-//
-//        // Access Token 유효기간 1일
-//        String accessToken = jwtUtil.generateToken(claim,1);
-//        // Refresh Token 유효기간 30일
-//        String refreshToken = jwtUtil.generateToken(claim,30);
-//
-//        // successToken, refreshToken이 포함된 JSON 응답 생성
-//        Gson gson = new Gson();
-//        Map<String, String> keyMap = Map.of("accessToken", accessToken, "refreshToken", refreshToken);
-//
-//        String jsonStr = gson.toJson(keyMap);
-//
-//        // JSON 응답을 출력에 작성
-//        response.getWriter().println(jsonStr);
-//    }
-//}
-//
+package com.example.lecture_B.security.handler;
+
+
+import com.example.lecture_B.dto.SignUpDTO;
+import com.example.lecture_B.entity.CustomUser;
+import com.example.lecture_B.util.JwtUtil;
+import com.google.gson.Gson;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+// 인증 성공 후 처리 작업을 담당
+@Log4j2
+@RequiredArgsConstructor
+public class UserLoginSuccessHandler implements AuthenticationSuccessHandler {
+
+    private final JwtUtil jwtUtil;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        log.info("Login Success Handler.............");
+
+        // 응답 콘텐츠 타입을 JSON으로 설정
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        log.info(authentication.toString());
+        log.info("Username: " + authentication.getName()); // username
+
+
+        String userId = ((CustomUser) authentication.getPrincipal()).getUserId();
+        log.info("Email: " + userId);
+        CustomUser customUser = (CustomUser) authentication.getPrincipal();
+        String nickname = customUser.getNickname();
+        log.info("userId: " + userId);
+
+        // 클레임 생성 : JWT 토큰에 포함될 클레임(사용자 정보)을 생성
+        // JWTUtil payload부분에서 .putall로 userid, nickname, roles 값이 들어감
+        Map<String, Object> claim = new HashMap<>();
+        claim.put("userId", userId);
+        claim.put("nickname", nickname);
+        claim.put("roles", authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toList()));
+
+        // Access Token 유효기간 1일
+        String accessToken = jwtUtil.generateToken(claim,1);
+        // Refresh Token 유효기간 30일
+        String refreshToken = jwtUtil.generateToken(claim,30);
+
+        // successToken, refreshToken이 포함된 JSON 응답 생성
+        Gson gson = new Gson();
+        Map<String, String> keyMap = Map.of("accessToken", accessToken, "refreshToken", refreshToken);
+
+        String jsonStr = gson.toJson(keyMap);
+
+        // JSON 응답을 출력에 작성
+        response.getWriter().println(jsonStr);
+    }
+}
+
